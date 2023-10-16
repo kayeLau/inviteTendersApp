db5ea6483f3187537f8a4b3390a55f7998271c97
nyada77 <1127015207@qq.com>
2023-10-16 15:35:01 +0800

    修改包的文件位置，添加todo模型默认值

--- api/routers/main.py
+++ api/routers/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from routers import bud
 from sql_app import models
 from sql_app.db import engine
+import uvicorn
 
 # 建表
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(bud.router)
+app.include_router(bud.app)
 
-
+if __name__ == '__main__':
+    uvicorn.run(app="bud:app", host="127.0.0.1", port=8000, reload=True)
 #  dev:py -3 -m uvicorn main:app --reload
