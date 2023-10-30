40385f0dfcc6dd1d8b2198dbf7a128e1d09e0d17
kayeLau <3236217797@qq.com>
2023-10-30 19:04:32 +0800

    新增爬虫中央控制

--- apiNode/spider/index.js
+++ apiNode/spider/index.js
@@ -1,81 +1,21 @@
-const puppeteer = require('puppeteer');
 
-(async () => {
-  let resultList = []
-  const Bud_controller = require('../controllers/bud_controller')
-  const bud_controller = new Bud_controller()
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
-  const page = await browser.newPage();
-  await page.evaluateOnNewDocument('const newProto = navigator.__proto__;delete newProto.webdriver;navigator.__proto__ = newProto;');
-  await page.goto('https://ecsg.com.cn/cms/NoticeList.html?id=1-1&typeid=4&word=&seacrhDate=');
-  await page.waitForSelector('#noticeListTBody tr')
 
-  await getHref()
-  await getContent()
-  await insertBud()
-
-  console.log(resultList)
-
-  browser.close();
-
-  // methods
-  async function getContent() {
-    for (let i = 0; i < resultList.length ; i++) {
-      await page.goto(resultList[i].data_href);
-      await page.waitForSelector('.edit-container > p')
-      const result = await page.$$eval('.edit-container > p', rows => {
-        let text = ''
-        rows.forEach((row) => {
-          const columns = row.querySelectorAll('span');
-          columns.forEach(item => {
-            text += item.innerText
-          })
-        })
-        return text + '\n'
-      })
-      resultList[i].bud_body = result
+(async() => {
+    const intervalTime = 1000 * 60 * 60
+    const scriptAbleHour = '08'
+    const ecsg = require('./ecsg')
+    ecsg()
+    
+    function isScriptAble(){
+        let time = new Date
+        let hour = time.getHours()
+        return hour === scriptAbleHour ? true : false
     }
-  }
 
 
-  async function getHref() {
-    const result = await page.$$eval('#noticeListTBody tr', rows => {
-      return Array.from(rows, row => {
-        const columns = row.querySelectorAll('td');
-        const data_href = row.querySelector('td > a').href
-        const bud_title = columns[0].innerText
-        const bud_unit = columns[1].innerText
-        const release_time = columns[2].innerText
-        const bud_body = ''
-        return { data_href, bud_title, bud_unit, release_time, bud_body }
-      });
-    });
-    resultList = result
-  }
-
-  async function insertBud(){
-    const data = resultList.map(item => {
-      return {
-        bud_title:item.bud_title,
-        bud_body:item.bud_body,
-        bud_table:item.bud_table,
-        release_time:item.release_time,
-        bud_unit:item.bud_unit || null,
-        bud_type:0, // 政府项目
-        pj_type:item.pj_type || 0,
-        bud_city:item.bud_city || null,
-        bud_contact:item.bud_contact || null,
-        bud_amount:item.bud_amount || null,
-        data_source:0, // 机器获取
-        data_href:item.data_href
-      }
-    })
-    await bud_controller.postInsertBudItems(data).then(res => {
-      console.log(res)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+    setInterval(() => {
+        if(isScriptAble){
+            ecsg()
+        }
+    },intervalTime)
 })();
