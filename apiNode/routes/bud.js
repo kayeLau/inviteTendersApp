ee89a5d3d13129cb79be6351f8605eba2ccf8c89
kayeLau <3236217797@qq.com>
2024-03-07 21:03:15 +0800

    delete

--- apiNode/routes/bud.js
+++ apiNode/routes/bud.js
@@ -7,11 +7,11 @@ const budModifyMethod = new BudModifyMethod()
 
 router.post('/getBudList',budModifyMethod.getBudList)
 
-router.post('/createBud',budModifyMethod.postCreateBud)
+// router.post('/createBud',budModifyMethod.postCreateBud)
 
-router.post('/updateBud',budModifyMethod.postUpdateBud)
+// router.post('/updateBud',budModifyMethod.postUpdateBud)
 
-router.post('/deleteBud',budModifyMethod.postDeleteBud)
+// router.post('/deleteBud',budModifyMethod.postDeleteBud)
 
 
 module.exports = router;
